6d799d2ed79731979ff991df4ed5753df18b1a8f
Polianskiy Artur <44229762+artum24@users.noreply.github.com>
2025-10-08 01:27:25 +0300

    fixed login flow

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -6,16 +6,13 @@ import LoginForm from '@/components/auth/login/LoginForm';
 export const metadata: Metadata = {
   title: 'Авторизація',
   description:
-    'Вхід в особистий кабінет сервісу Remo CRM: є можливість авторизації через Google або за електронною поштою.',
+    'Вхід в особистий кабінет сервісу: є можливість авторизації через Google або за електронною поштою.',
 };
 
 export default function LoginPage() {
   return (
     <div className="text-start flex flex-col gap-5">
       <h1 className="text-3xl font-bold text-gray-800">Увійти</h1>
-      <p className="text-gray-500 text-sm">
-        Огляд клієнтів, їхніх автомобілів, контактної інформації та статусів обслуговування.
-      </p>
       <LoginForm />
       <p className="text-gray-500">
         Не маєте облікового запису?
